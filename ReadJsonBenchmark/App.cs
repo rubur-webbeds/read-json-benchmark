7ae11b5dfccd8aa91d319f97a777f3e9a6bffa18
Ruben Bustos <ruben_b_r_@hotmail.com>
2018-11-20 23:18:35 +0100

    First commit

---
+++ ReadJsonBenchmark/App.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ReadJsonBenchmark
+{
+    public class App
+    {
+        private readonly IMemoryCache _cache;
+
+        public App(IMemoryCache cache)
+        {
+            _cache = cache;
+
+            SaveJsonOnCache("Lorem", ReadJsonFromDisk("lorem_min.json"));
+        }
+
+        public void SaveJsonOnCache(string cacheKey, string file)
+        {
+            _cache.Set<string>(cacheKey, file);
+        }
+
+        public string ReadJsonFromCache(string cacheKey)
+        {
+            return _cache.Get<string>(cacheKey);
+        }
+
+        public string ReadJsonFromDisk(string path)
+        {
+            using (var reader = new StreamReader(path))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ ReadJsonBenchmark/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ReadJsonBenchmark
+{
+    class Program
+    {
+        private static IMemoryCache _cache;
+
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddMemoryCache()
+                .BuildServiceProvider();
+
+            _cache = serviceProvider.GetService<IMemoryCache>();
+
+            var summary = BenchmarkRunner.Run<Program>();
+        }
+
+        static Program()
+        {
+            SaveJsonOnCache("Lorem", ReadJsonFromDisk("lorem_min.json"));
+        }
+
+        public static void SaveJsonOnCache(string cacheKey, string file)
+        {
+            _cache.Set<string>(cacheKey, file);
+        }
+
+        [Benchmark]
+        public static string ReadJsonFromCache(string cacheKey)
+        {
+            return _cache.Get<string>(cacheKey);
+        }
+
+        [Benchmark]
+        public static string ReadJsonFromDisk(string path)
+        {
+            using (var reader = new StreamReader(path))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
